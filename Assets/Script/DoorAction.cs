e2f7151e1b046376d1fa31c0486072659ee655c8
lungnahahd <67555400+lungnahahd@users.noreply.github.com>
2021-07-15 22:07:05 +0900

    Animator Script 작성

---
+++ Assets/Script/DoorAction.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DoorAction : MonoBehaviour
+{
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.transform.tag == "Player")
+        {
+            transform.GetComponent<Animator>().SetBool("isOpen", true);
+        }
+    }
+    private void OnTriggerExit(Collider other)
+    {
+        transform.GetComponent<Animator>().SetBool("isOpen", false);
+    }
+}
